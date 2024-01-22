fc86b3c7ff096fde30bbeb3e2ea548b1a29d8c0c
= <=>
2024-01-22 19:50:44 -0300

    database config updated

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USERNAME, 
   process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST,
-    dialect: "mysql",
-    port: 3306,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT,
   });
 
   export default sequelize;
