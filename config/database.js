709c774b7ac4b9d27f80d3a9f964411f9d94ca4e
= <=>
2024-01-23 21:17:54 -0300

    database config updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
+import mysql2 from 'mysql2';
+
 import dotenv from "dotenv"
 dotenv.config();
+
 const sequelize = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USERNAME, 
@@ -8,6 +11,7 @@ const sequelize = new Sequelize(
   host: process.env.DATABASE_HOST,
     dialect: "mysql",
     port: process.env.DATABASE_PORT,
+    dialectModule: mysql2
   });
 
   export default sequelize;
