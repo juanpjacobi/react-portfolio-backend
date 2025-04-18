d75f37e928f0866ea1a3d9f6ffb7f882dc3bc5e9
= <=>
2025-04-17 22:21:10 -0300

    index updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
+import sequelize from "./config/database.js";
 
 const port = process.env.PORT || 4000;
 
@@ -10,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
+
+
+
 app.listen(port, () => {
   console.log(`Server corriendo en puerto ${port}`);
 });
