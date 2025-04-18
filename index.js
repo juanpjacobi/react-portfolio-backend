eb35d4d82b07f394b9de542da8360afd84e4950d
= <=>
2025-04-17 22:33:45 -0300

    updated

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/index.js";
-import sequelize from "./config/database.js";
 
 const port = process.env.PORT || 4000;
 
@@ -13,16 +12,7 @@ app.use(express.json());
 app.use("/api", router);
 
 
-sequelize.sync({ alter: true })
-  .then(() => {
-    console.log("✅ Tablas sincronizadas (alter)");
-    app.listen(port, () => {
-      console.log(`🚀 Server corriendo en puerto ${port}`);
-    });
-  })
-  .catch(err => {
-    console.error("❌ Error al sincronizar la DB:", err);
-  });
+
 app.listen(port, () => {
   console.log(`Server corriendo en puerto ${port}`);
 });
